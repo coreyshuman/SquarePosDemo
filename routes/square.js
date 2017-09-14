2d96c732ece37d77a736e7e24b0e3971e4f2a1fb
Corey Shuman <qddrum@sbcglobal.net>
2017-09-13 17:04:25 -0700

    fixed callback parse

--- routes/square.js
+++ routes/square.js
@@ -4,7 +4,7 @@ var square = require('../square-response');
 
 /* get callback from square pos. */
 router.get('/callback', function(req, res, next) {
-    var data = req.query.data;
+    var data = JSON.parse(req.query.data);
     var squareResponse = new square(data);
     res.render('callback', { status: squareResponse.isSuccess(), errorMessage: squareResponse.getErrorMessage() });
 });
